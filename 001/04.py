589c11abc8a8d6ebc3a2711f079ed1f3a2b4867d
Ishanya Deogade <ishanyadeogade@gmail.com>
2026-09-11 13:21:58 +0530

    python-core-practice

--- 001/04.py
+++ 001/04.py
@@ -1,11 +1,16 @@
 
 base = int(input("Enter a base : "))
 index = int(input("Enter an index : "))
-p = 1
-while index > 0:
-    p = p * base;
-    index-=1
-print(p)
+if index < 0 :
+    print("Index should be a positive number")
+elif index == 0 :
+    print(1)
+else :
+    p = 1
+    while index > 0:
+        p = p * base;
+        index-=1
+    print(p)
 """
 Enter a base : 3
 Enter an index : 4
---
+++ 002/01.py
@@ -0,0 +1,11 @@
+
+num = int(input("Enter a number : "))
+count = 0
+while num > 0 :
+    num = num // 10 
+    count+=1
+print(count)
+"""
+Enter a number : 0432 
+3
+"""
