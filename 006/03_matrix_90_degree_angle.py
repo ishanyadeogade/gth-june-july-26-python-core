2e0f9f70c9ce693bae53167a79cea5b5bc41e720
Ishanya Deogade <ishanyadeogade@gmail.com>
2026-09-12 21:35:20 +0530

    core python practice

---
+++ 006/03_matrix_90_degree_angle.py
@@ -0,0 +1,33 @@
+
+i = 1
+while i <= 5:
+    j = 1
+    while j <= 5:
+        if j==1:
+            print("*",end=" ")
+        elif i==5:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+        j += 1
+    print()
+    i += 1
+
+"""
+*         
+*         
+*         
+*         
+* * * * * 
+
+
+i = 1
+while i <= 5:
+    j = 1
+    while j == 1 or ( i==5 and j <= 5 ):
+        print("*", end=" ")
+        j += 1
+    print()
+    i += 1
+   
+"""   
