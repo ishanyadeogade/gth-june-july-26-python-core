d018d55e4005fcb1249ee2930dd4a3594bb162e6
Ishanya Deogade <ishanyadeogade@gmail.com>
2026-09-13 06:30:25 +0530

    core python practice

--- 006/pattern_assignment/02.py
+++ 006/pattern_assignment/02.py
@@ -12,3 +12,21 @@ Q2. Write a java program to print this pattern.
 
 """
 
+i = 1
+while i <= 5 :
+    j = 1
+    while j <= 5 :
+        """if i == 1 or i == 5 :
+            print("*",end = " ")
+        elif j == 1 or  j == 5 :
+            print("*",end = " ")
+        """
+        if ( i == 1 or i == 5 ) or (j == 1 or  j == 5) :
+            print("*",end = " ")
+        else :
+            print(" ",end = " ")
+        j += 1
+    i += 1
+    print()
+    
+
