d93c1ca5d162992f398f31266e94a3802c6672c4
Ishanya Deogade <ishanyadeogade@gmail.com>
2026-09-13 11:22:44 +0530

    core python practice

---
+++ 007/01.py
@@ -0,0 +1,25 @@
+
+i=1
+while i<=5:
+    j=1
+    while j<=5:
+        if j>=i :
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+        j+=1
+    i+=1
+    print()
+    
+"""
+
+* * * * * 
+  * * * * 
+    * * * 
+      * * 
+        * 
+        
+"""
+
+        
+        
