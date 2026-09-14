672c3309d2851d7519eafec7f36e09670a65804b
Ishanya Deogade <ishanyadeogade@gmail.com>
2026-09-15 01:43:29 +0530

    core python practice

---
+++ 009/01.py
@@ -0,0 +1,14 @@
+values=range(10)
+print(values)
+print(range(5,10))
+
+print(range(1,10,2)) 
+
+"""
+range(0, 10) # 0,1,2,3,4,5,6,7,8,9
+range(5, 10) # 5,6,7,8,9
+range(1, 10, 2) # 1,3,5,7,9
+
+
+
+"""
---
+++ 009/02.py
@@ -0,0 +1,20 @@
+
+limits = range(10)
+
+for i in limits:
+    print(i)
+
+"""
+
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+
+"""
